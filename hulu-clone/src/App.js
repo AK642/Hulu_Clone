3e74210910886d293bb976ecce937a35af43d8d2
AK642 <ajayv6583@gmail.com>
2020-12-06 09:32:28 +0530

    first commit

---
+++ hulu-clone/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Results from './Results';
+import requests from "./requests";
+import { useState } from 'react';
+
+function App() {
+
+  const [selectedOption, setSelectedOption] = useState(requests.fetchTrending);
+
+  return (
+    <div className="app">
+
+        <Header />
+
+        <Nav setSelectedOption={setSelectedOption}/>
+
+        <Results selectedOption={selectedOption}/>
+
+    </div>
+  );
+}
+
+export default App;
